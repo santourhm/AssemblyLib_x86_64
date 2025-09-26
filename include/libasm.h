9b226c0b474aa6ba4ffe014a5ecb5d5296a381d1
Hamza Santoura <santourahamza.cpge@gmail.com>
2025-09-26 09:09:26 -0500

    adding strcmp function

--- include/libasm.h
+++ include/libasm.h
@@ -1,9 +1,12 @@
 #ifndef LIBASM_H
 #define LIBASM_H
+#include <unistd.h>
 
 #include <stddef.h> 
 
-size_t ft_strlen(const char *s);
-char *ft_stpcpy(char *restrict dst, const char *restrict src);
+size_t  ft_strlen(const char *s);
+char   *ft_stpcpy(char *restrict dst, const char *restrict src);
+int     ft_strcmp(const char *s1, const char * s2);
+size_t ft_write(int fd, const void * buf, size_t count);
 
 #endif
