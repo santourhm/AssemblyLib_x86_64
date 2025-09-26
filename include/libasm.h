fcf108a5681fade38253b88e1faf2c8c33028b01
Hamza Santoura <santourahamza.cpge@gmail.com>
2025-09-26 18:13:44 -0500

    adding strdup and structiong correctly the project

--- include/libasm.h
+++ include/libasm.h
@@ -2,11 +2,11 @@
 #define LIBASM_H
 #include <unistd.h>
 
-#include <stddef.h> 
-
 size_t  ft_strlen(const char *s);
-char   *ft_stpcpy(char *restrict dst, const char *restrict src);
-int     ft_strcmp(const char *s1, const char * s2);
-size_t ft_write(int fd, const void * buf, size_t count);
+char   *ft_strcpy(char *restrict dst, const char *restrict src);
+int     ft_strcmp(const char *s1, const char *s2);
+ssize_t ft_write(int fd, const void *buf, size_t count);
+char    * ft_strdup(const char * s);
+
 
 #endif
